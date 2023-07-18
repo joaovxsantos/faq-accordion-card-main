3459bae57cf474223d57e90b6ad23a8948478cb6
João Vitor <joaovitor55670@gmail.com>
2023-07-18 17:21:35 -0300

    JS OK

---
+++ script.js
@@ -0,0 +1,18 @@
+const arrow = document.querySelectorAll('[name="chevron-down-outline"]');
+const card = document.querySelectorAll('.card');
+const title = document.querySelectorAll('.title');
+const names = ['chevron-down-outline', 'chevron-up-outline']
+
+arrow.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        card[index].children[1].classList.toggle('on');
+
+        if (card[index].children[1].classList[1] === 'on') {
+            element.name = 'chevron-up-outline';
+            title[index].style = 'font-weight:700';
+        } else {
+            element.name = 'chevron-down-outline';
+            title[index].style = 'font-weight:300';
+        }
+    })
+})
